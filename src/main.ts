f5f431b2bc852be45686cf752a3c44174b66f36c
Mishahinton <misha.hinton@education.nsw.gov.au>
2023-05-02 17:38:24 +1000

    array for temp, api error check, set date,min,max

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,42 @@ import './style.css';
 //   html: string;
 //   domNodes: any[];
 // }
+interface WeatherDay {
+  date: number;
+  max: number;
+  min: number;
+}
 
+type Weather = Array<WeatherDay>;
 const weather: Weather = [];
 
-const getWeather = async () => {
-  const responce = await (
-    await fetch(
-      'https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&hourly=temperature_2m&forecast_days=16&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min&start_date=2023-03-26&end_date=2023-05-06'
-    )
-  ).json();
-  console.log(responce);
+const getWeather = async (startDate: string, endDate: string) => {
+  try {
+    const response = await (
+      await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&hourly=temperature_2m&forecast_days=16&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min&start_date=${startDate}&end_date=${endDate}`
+      )
+    ).json();
+    console.log(response);
+    //checking if the api returns an error, specific to this api.
+    if (response.error) {
+      return;
+    }
+
+    const dailyTimes: Array<string> = response.daily.time;
+
+    for (let i = 0; i < dailyTimes.length; i++) {
+      weather.push({
+        date: new Date(dailyTimes[i]).setHours(0, 0, 0, 0),
+        max: response.daily.temperature_2m_max[i],
+        min: response.daily.temperature_2m_min[i],
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
+
 (async () => {
   let calendar = new Calendar(document.getElementById('app')!, {
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
@@ -38,22 +63,23 @@ const getWeather = async () => {
       };
     },
   });
+
   const getISO = (date: Date) => {
     return calendar.formatIso(date).split('T')[0];
   };
+
+  const startDate = getISO(calendar.view.activeStart);
+  const nowDate = new Date().getTime();
+  const calendarEndDate = calendar.view.activeEnd.getTime();
+  const fortnightFromNow = nowDate + 1209600000;
+
+  const endDate =
+    fortnightFromNow > calendarEndDate ? calendarEndDate : fortnightFromNow;
+
+  await getWeather(startDate, getISO(new Date(endDate)));
+
   console.log(calendar.view.activeStart.toISOString(), calendar.view.activeEnd);
   calendar.render();
 
   console.log(getISO(calendar.view.activeStart));
 })();
-
-interface WeatherDay {
-  date: number;
-  max: number;
-  min: number;
-}
-
-type Weather = Array<WeatherDay>;
-
-weather[0].max;
-
--- test.cjs
+++ test.cjs
@@ -1,7 +1,7 @@
 (async () => {
   const responce = await (
     await fetch(
-      'https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&hourly=temperature_2m&forecast_days=16&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min&start_date=2023-03-26&end_date=2023-05-06'
+      `https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&hourly=temperature_2m&forecast_days=16&timezone=auto&daily=temperature_2m_max&daily=temperature_2m_min&start_date=${startDate}&end_date=${endDate}`
     )
   ).json();
   console.log(responce);
