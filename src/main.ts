9e437fa7eac2944bae13fa88c18d86a1245505a8
Mishahinton <misha.hinton@education.nsw.gov.au>
2023-06-21 19:00:47 +1000

    <related_message_about_staged_files_changes>

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,8 @@ const getWeather = async (startDate: string, endDate: string) => {
     for (let i = 0; i < dailyTimes.length; i++) {
       weather.push({
         date: new Date(dailyTimes[i]).setHours(0, 0, 0, 0),
-        max: response.daily.temperature_2m_max[i],
-        min: response.daily.temperature_2m_min[i],
+        max: Math.round(response.daily.temperature_2m_max[i]),
+        min: Math.round(response.daily.temperature_2m_min[i]),
       });
     }
   } catch (error) {
@@ -140,6 +140,7 @@ const getWeather = async (startDate: string, endDate: string) => {
   }
 
   const calendar = new Calendar(document.getElementById('calendar')!, {
+    //    aspectRatio: 1,
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
     customButtons: {
       addEvent: {
@@ -159,12 +160,42 @@ const getWeather = async (startDate: string, endDate: string) => {
      * @returns Cell content.
      */
     dayCellContent: (renderProps) => {
-      // console.log(renderProps);
+      const dayWeather = weather.find(
+        (o) => o.date === renderProps.date.getTime()
+      );
+
       return {
-        html: `<div class="datetemp">
-        <div class="date"><span>${renderProps.dayNumberText}</span></div>
-        <div class="temp"><span></span></div>
-      </div>`,
+        html: dayWeather
+          ? `<div class="datetemp">
+              <div class="date">
+                <span>${renderProps.dayNumberText}</span>
+              </div>
+              <div class="weather">
+                <div>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585A1.5 1.5 0 0 1 5 12.5z"/>
+                      <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1.293l.646-.647a.5.5 0 0 1 .708.708L9 5.207v1.927l1.669-.963.495-1.85a.5.5 0 1 1 .966.26l-.237.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.884.237a.5.5 0 1 1-.26.966l-1.848-.495L9.5 8l1.669.963 1.849-.495a.5.5 0 1 1 .258.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.237.883a.5.5 0 1 1-.966.258L10.67 9.83 9 8.866v1.927l1.354 1.353a.5.5 0 0 1-.708.708L9 12.207V13.5a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5z"/>
+                    </svg>
+                  </span>
+                  <span>${dayWeather.min}</span>
+                </div>
+                <div>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585A1.5 1.5 0 0 1 5 12.5z"/>
+                      <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5zm4.243 1.757a.5.5 0 0 1 0 .707l-.707.708a.5.5 0 1 1-.708-.708l.708-.707a.5.5 0 0 1 .707 0zM8 5.5a.5.5 0 0 1 .5-.5 3 3 0 1 1 0 6 .5.5 0 0 1 0-1 2 2 0 0 0 0-4 .5.5 0 0 1-.5-.5zM12.5 8a.5.5 0 0 1 .5-.5h1a.5.5 0 1 1 0 1h-1a.5.5 0 0 1-.5-.5zm-1.172 2.828a.5.5 0 0 1 .708 0l.707.708a.5.5 0 0 1-.707.707l-.708-.707a.5.5 0 0 1 0-.708zM8.5 12a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5z"/>
+                    </svg>
+                  </span>
+                  <span>${dayWeather.max}</span>
+                </div>
+              </div>
+            </div>`
+          : `<div class="datetemp">
+              <div class="date">
+                <span>${renderProps.dayNumberText}</span>
+              </div>
+            </div>`,
       };
     },
   });
